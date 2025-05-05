49acf39bafbb4754c380cebe2a350e6ad92bcdc0
T189216 <tus0926@naver.com>
2025-05-06 00:20:32 +0900

    setting

---
+++ src/main/java/com/ll/in_pli01/in_pli01Application.java
@@ -0,0 +1,13 @@
+package com.ll.in_pli01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class in_pli01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(in_pli01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/in_pli01/inpli01ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.in_pli01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class inpli01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
